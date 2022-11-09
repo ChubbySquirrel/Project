fc3f145e8b87c5e528dccd115d498797c373ec15
ChubbySquirrel <grantfact@gmail.com>
2022-11-09 15:53:15 -0500

    hello

---
+++ Class Codes/ode_bvp.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+#==============================================================
+# function sign = lunarlanding(x,y,dx)
+#
+# Returns sign of y[0], i.e. of the altitude above ground.
+# See ode_test.py, specifically ode_init.
+#
+#--------------------------------------------------------------
+
+def lunarlanding(y):
+    return np.sign(y[0])
+
